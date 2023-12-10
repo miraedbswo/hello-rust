4747b3cb139f94ff53e183bc6c15a21f334a7b40
miraedbswo <miraedbswo@gmail.com>
2023-12-10 22:38:11 +0900

    code: if let 추가

---
+++ hello-rust/src/if_let.rs
@@ -0,0 +1,20 @@
+let some_u8_value = Some(0u8);
+match some_u8_value {
+    Some(3) => println!("three"),
+}
+
+if let Some(3) = some_u8_value {
+    println!("three");
+}
+
+// if let and else
+match some_u8_value {
+    Some(3) => println!("three"),
+    _ => println!("not three"),
+}
+
+if let Some(3) = some_u8_value {
+    println!("three");
+} else {
+    println!("not three");
+}
